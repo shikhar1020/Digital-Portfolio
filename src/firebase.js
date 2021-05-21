a3fb48d143f7a1466e3d4680231020a3b4055c6e
Shikhar Srivastava <54438024+shikhar1020@users.noreply.github.com>
2021-05-22 03:37:14 +0530

    deploy 001

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAhMJP-Y4oV5BkzJPKF0VwvDtN2G5kwcic",
+    authDomain: "shikhar-srivastava.firebaseapp.com",
+    projectId: "shikhar-srivastava",
+    storageBucket: "shikhar-srivastava.appspot.com",
+    messagingSenderId: "295989535564",
+    appId: "1:295989535564:web:7c4eab441177359db4e5e9",
+    measurementId: "G-LLQFGVR1BG"
+  };
