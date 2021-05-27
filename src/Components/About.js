0f4617407492a00e0999a76566e229a13372d958
Shikhar Srivastava <54438024+shikhar1020@users.noreply.github.com>
2021-05-28 00:06:24 +0530

    downloadble and viewable

--- src/Components/About.js
+++ src/Components/About.js
@@ -47,7 +47,7 @@ const About = ({ data }) => {
             </div>
             <div className="columns download">
               <p>
-                <a href={resumeDownload} className="button">
+                <a href={resumeDownload} target="_blank" rel="noopener noreferrer" className="button" download="Shikhar_Resume">
                   <i className="fa fa-download"></i>Download Resume
                 </a>
               </p>
